b5127b39d251e86736c1feaf77f08a422e6a9f35
Andreas Lorenz <info@andreaslorenz.tech>
2025-05-27 17:06:19 +0200

    Updated config

--- delete-blue-deployment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Input arguments
-REGION="${1:-us-east-1}"  # Default to us-east-1 if not provided
-VERSION="${2:-latest}"  # Version of the image to deploy
-
-bash delete-blue-green.sh "$REGION" "blue" "$VERSION"
